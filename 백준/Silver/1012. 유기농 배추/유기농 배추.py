6c35334a497238ffa7bfc4b5b0f68de1bc314945
soobin <79037489+sxxcxng@users.noreply.github.com>
2024-05-19 01:35:51 +0900

    [Silver II] Title: 유기농 배추, Time: 336 ms, Memory: 34192 KB -BaekjoonHub

---
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -0,0 +1,28 @@
+from collections import deque
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1] # 상하좌우
+
+t = int(input())
+for ___ in range(t):
+    m, n, k = map(int,input().split())
+    arr = [[0]*m for _ in range(n)]
+    for i in range(k):
+        y, x = map(int,input().split())
+        arr[x][y] = 1
+    check = [[0]*m for _ in range(n)]
+    val = 1
+
+    q = deque()
+    for i in range(n):
+        for j in range(m):
+            if check[i][j] == 0 and arr[i][j] == 1:
+                check[i][j] = val
+                q.append((i,j))
+                while q:
+                    x, y = q.popleft()
+                    for k in range(4):
+                        nx, ny = x + dx[k], y + dy[k]
+                        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] and not check[nx][ny]:
+                            check[nx][ny] = val
+                            q.append((nx,ny))
+                val += 1
+    print(val - 1)
