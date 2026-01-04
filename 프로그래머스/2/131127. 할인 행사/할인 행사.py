419d5840e2dd8323fd90b95b140eed67bca3ceea
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-04 18:24:17 +0900

    [level 2] Title: 할인 행사, Time: 150.26 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.py
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.py
@@ -2,15 +2,14 @@ from collections import Counter
 
 def solution(want, number, discount):
     answer = 0
+    # 빈 딕셔너리 배열 생성
     dic = {}
     
-    # 원하는 상품 딕셔너리 생성
     for i in range(len(want)):
         dic[want[i]] = number[i]
 
-    # 10일씩 슬라이딩
-    for i in range(len(discount) - 9):
-        if dic == Counter(discount[i : i + 10]): 
+    for i in range(len(discount)-9):
+        if dic == Counter(discount[i:i+10]): 
             answer += 1
 
     return answer
