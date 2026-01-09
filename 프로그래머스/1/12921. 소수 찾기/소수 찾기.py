4d96c73906f1e6216d7117dda34a81115a45e06a
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-09 16:02:36 +0900

    [level 1] Title: 소수 찾기, Time: 3396.70 ms, Memory: 9.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    ans = []
+    
+    if n == 2:
+        return 1
+    
+    for i in range(2, n + 1):
+        is_prime = True
+        for j in range(2, int(i ** 0.5) + 1):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime == True:
+            answer += 1
+    return answer
