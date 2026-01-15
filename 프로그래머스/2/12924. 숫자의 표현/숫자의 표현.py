5101126ea794500552c5d96d175553260b92d62b
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-15 15:08:25 +0900

    [level 2] Title: 숫자의 표현, Time: 6.19 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,13 +1,13 @@
 def solution(n):
-    cnt = 0
-
+    ans = 0
     for i in range(1, n + 1):
-        total = 0
+        cnt = 0
         for j in range(i, n + 1):
-            total += j
-            if total == n:
-                cnt += 1
+            cnt += j
+            if cnt == n:
+                ans += 1
                 break
-            elif total > n:
+            elif cnt > n:
                 break
-    return cnt
+            
+    return ans
