89f70d13ffce1ce82926b56941a021f3ac197170
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-15 15:25:17 +0900

    [level 2] Title: 짝지어 제거하기, Time: 204.77 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.py
@@ -1,11 +1,11 @@
 def solution(s):
     stack = []
-
-    for char in s:
-        if stack and stack[-1] == char:
-            stack.pop()
-        else:
-            stack.append(char)
+    
+    for i in s[:]:
+        stack.append(i)
+        if len(stack) > 1 and stack[-1] == stack[-2]:
+            del stack[-1]
+            del stack[-1]
 
     if len(stack) == 0:
         return 1
