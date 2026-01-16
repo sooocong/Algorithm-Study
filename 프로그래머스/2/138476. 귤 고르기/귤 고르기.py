a990c333bc66272ad5b324e3f89ee9e44d88cd52
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-16 13:41:26 +0900

    [level 2] Title: 귤 고르기, Time: 58.36 ms, Memory: 20.5 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,19 +1,22 @@
 def solution(k, tangerine):
     answer = 0
-    d = {}
+    tan_dict = {}
     
-    for t in tangerine:
-        if t in d:
-            d[t] += 1
+    # 딕셔너리로 만들고, 개수 기준 역순으로 정렬
+    # k개까지 ..
+    for i in range(len(tangerine)):
+        if tangerine[i] not in tan_dict:
+            tan_dict[tangerine[i]] = 1
         else:
-            d[t] = 1
-            
-    d = sorted(d.items(), key = lambda x: x[1], reverse = True)
-    
-    for i in d:
-        k -= i[1]
+            tan_dict[tangerine[i]] += 1
+
+    lst = list(tan_dict.items())
+    lst.sort(key=lambda x:x[1], reverse = True)
+
+    i = 0
+    while k > 0:
         answer += 1
-        if k <= 0:
-            break
-    
+        k -= lst[i][1]
+        i += 1
+        
     return answer
