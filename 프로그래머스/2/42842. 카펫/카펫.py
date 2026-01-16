2521a105a4e71f2632c7d194fcd76038cb4c263f
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-16 12:34:11 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -2,9 +2,10 @@ def solution(brown, yellow):
     answer = []
     total = brown + yellow
     
-    for h in range(1, total + 1):
+    for h in range(3, total // 2):
         if total % h == 0:
             w = total // h
-            if w >= h:
-                if (w - 2) * (h - 2) == yellow:
-                    return [w, h]
+            if (w - 2) * (h - 2) == yellow and 2 * (w - 1) + 2 * (h - 1) == brown:
+                answer.append(w)
+                answer.append(h)
+                return answer
