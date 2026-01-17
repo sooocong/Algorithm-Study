139266cadcb17b844d0d22595e90ce95f96bb9ab
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-17 17:31:36 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.03 ms, Memory: 9.12 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.py
@@ -1,6 +1,15 @@
 def solution(n, words):
-    d = {}
+    dict = {}
+    dict[words[0]] = 1
     for i in range(1, len(words)):
-        if words[i] in words[:i] or words[i][0] != words[i - 1][-1]:
-            return [i % n + 1, i // n + 1]
-    return [0, 0]
+        if words[i] not in dict:
+            dict[words[i]] = 1
+            if words[i - 1][-1] != words[i][0]:
+                return [i % n + 1, i // n + 1] # 번호, 차례
+            else:
+                continue
+        else:
+            dict[words[i]] += 1
+            return [i % n + 1, i // n + 1] # 번호, 차례
+        
+    return [0, 0] # 탈락자 x
