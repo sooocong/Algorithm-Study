5429d125d9653c26507dbd39bec0fbc11565f635
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-17 17:02:56 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.34 ms, Memory: 9.2 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.py
@@ -1,15 +1,11 @@
-# 유형 : DP(피보나치)
 def solution(n):
-    if n == 1:
-        return 1
-    if n == 2:
-        return 2
-    
+
     dp = [0] * (n + 1)
     dp[1] = 1
-    dp[2] = 2
+    if n >= 2:
+        dp[2] = 2
     
     for i in range(3, n + 1):
         dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567
-        
+    
     return dp[n]
