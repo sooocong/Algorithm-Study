bcf147f4d467229a40ae1e27ee1faadac5ca6aa4
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-17 12:15:59 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.02 ms, Memory: 9.17 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.py
@@ -1,13 +1,11 @@
 def solution(n):
     ans = 0
-    # 뒤로 가는 걸로 생각하기
-    # 홀수면 1 빼고(1만큼 점프), 짝수면 2로 나누기
-    
+
     while n != 0:
-        if n % 2 != 0:
+        if n % 2 == 0:
+            n //= 2
+        else:
             n -= 1
             ans += 1
-        else:
-            n //= 2
     
     return ans
