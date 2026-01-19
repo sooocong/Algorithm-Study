d43fd70c55da36f77e0f108b56101eb47e306819
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-19 11:54:49 +0900

    [level 2] Title: n^2 배열 자르기, Time: 41.73 ms, Memory: 17.3 MB -BaekjoonHub

--- 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
+++ 프로그래머스/2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -1,8 +1,7 @@
 def solution(n, left, right):
-    # 각 배열의 원소는 각 행, 열의 max + 1
-    answer = []
+    result = []
+
+    for i in range(left, right + 1):
+        result.append(max(i // n, i % n) + 1)
     
-    for k in range(left, right + 1):
-        answer.append(max(k // n, k % n) + 1)
-        
-    return answer
+    return result
