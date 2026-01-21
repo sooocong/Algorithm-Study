a897a71ea16a90bd57dc2f8f53179b76b78c69ef
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-21 15:00:00 +0900

    [level 2] Title: 전화번호 목록 -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.py
@@ -1,8 +1,7 @@
 def solution(phone_book):
-    
     phone_book.sort()
-    for i in range(len(phone_book) - 1):
-        if phone_book[i] in phone_book[i + 1][:len(phone_book[i])]:
-            return False
     
+    for i in range(len(phone_book) - 1):
+        if phone_book[i + 1].startswith(phone_book[i]):
+            return False
     return True
