b142e6cd80c1611d8d388bc229354936283900e8
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-21 14:07:17 +0900

    [level 2] Title: 기능개발, Time: 0.08 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,20 +1,23 @@
-from collections import deque
-
 def solution(progresses, speeds):
-    q = deque()
     
-    for p, s in zip(progresses, speeds):
-        q.append((100 - p + s - 1) // s)
-    
+    for i in range(len(progresses)):
+        mod = (100 - progresses[i]) % speeds[i]
+        progresses[i] = (100 - progresses[i]) // speeds[i]
+        if mod != 0:
+            progresses[i] += 1
+            
     result = []
-    while q:
-        day = q.popleft()
-        cnt = 1
-        
-        while q and q[0] <= day:
-            q.popleft()
-            cnt += 1
-        
-        result.append(cnt)
+    answer = 1
+    max_num = 0
+    for i in range(len(progresses) - 1):
+        max_num = max(max_num, progresses[i])
+        if progresses[i] < progresses[i + 1] and max_num < progresses[i + 1]:
+            result.append(answer)
+            answer = 1
+        else:
+            answer += 1
+            
+    if len(progresses) != sum(result):
+        result.append(len(progresses) - sum(result))
     
     return result
