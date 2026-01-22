3777c1515d64f3248610dcc0c46b7d54b9a77267
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-22 14:25:57 +0900

    [level 2] Title: 튜플, Time: 359.90 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/2/64065. 튜플/튜플.py
+++ 프로그래머스/2/64065. 튜플/튜플.py
@@ -1,15 +1,14 @@
 def solution(s):
-    sets = s[2:-2].split("},{")
-    sets = [list(map(int, x.split(","))) for x in sets]
-    sets.sort(key=len) # 원소 개수 기준으로 오름차순 정렬
-    
+    s_list = s[2:-2]
+    result = s_list.split("},{")
+    result = [list(map(int, r.split(","))) for r in result]
+    result.sort(key=len)
     answer = []
-    seen = set()
-
-    for group in sets:
-        for num in group:
-            if num not in seen:
-                answer.append(num)
-                seen.add(num)
+    
+    for res in result:
+        for r in res:
+            if r not in answer:
+                answer.append(r)
+    
     
     return answer
