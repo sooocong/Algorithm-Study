311200bb01a45f944727bfe99582b1cc8b200f6e
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-22 14:03:43 +0900

    [level 2] Title: 롤케이크 자르기, Time: 757.49 ms, Memory: 49.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -2,16 +2,15 @@ from collections import Counter
 
 def solution(topping):
     answer = 0
-    left = set()
     right = Counter(topping)
+    left = set()
     
-    for t in topping[:-1]:
+    for t in topping:
         left.add(t)
         right[t] -= 1
-        
         if right[t] == 0:
             del right[t]
         if len(left) == len(right):
             answer += 1
-            
+    
     return answer
