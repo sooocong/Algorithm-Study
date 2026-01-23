7e84c6b5836389b97b4331a499dd7b4fad140461
soobin <79037489+sooocong@users.noreply.github.com>
2026-01-23 20:55:15 +0900

    [level 2] Title: 타겟 넘버, Time: 382.62 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,36 +1,18 @@
 def solution(numbers, target):
     answer = 0
-    leaves = [0]
-    
-    for number in numbers:
-        tmp = []
-        for parent in leaves:
-            tmp.append(parent + number)
-            tmp.append(parent - number)
-        leaves = tmp
-        
-    for leaf in leaves:
-        if target == leaf:
-            answer += 1
+    index = 0
+    current_sum = 0
 
+    def dfs(index, current_sum):
+        nonlocal answer
+        
+        if index == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            dfs(index + 1, current_sum + numbers[index])
+            dfs(index + 1, current_sum - numbers[index])
+               
+    dfs(0, 0)
+    
     return answer
-
-# def solution(numbers, target):
-#     count = 0
-    
-#     def dfs(idx, total):
-#         nonlocal count # dfs 안에서 solution 함수의 count 변수를 수정할 수 있게 해야함
-        
-#         if idx == len(numbers):
-#             if target == total:
-#                 count += 1
-        
-#             return
-        
-#         dfs(idx + 1, total + numbers[idx])
-#         dfs(idx + 1, total - numbers[idx])
-    
-    
-#     # dfs(0, 0)
-    
-#     return count
