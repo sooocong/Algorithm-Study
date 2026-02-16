1968b3ce53bf76cb0bcbca47f913b87e86fefcef
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-16 16:51:08 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -11,24 +11,18 @@ for _ in range(N):
 
 olimpic.sort(key=lambda x: (x[1], x[2], x[3]), reverse = True)
 
-res = 1
-same = 0
+rank = 1
 for i in range(len(olimpic)):
-    if i == 0:
+    if i >= 1:
+        if olimpic[i][1:] == olimpic[i - 1][1:] and olimpic[i][0] == K:
+            print(rank)
+            break
+        if olimpic[i][1:] != olimpic[i - 1][1:]:
+            rank = i + 1
+            if olimpic[i][0] == K:
+                print(rank)
+                break
+    else:
         if olimpic[i][0] == K:
             print(1)
             break
-    else:
-        if olimpic[i][0] == K:
-            if olimpic[i][1:] != olimpic[i - 1][1:]:
-                res += 1
-                print(res + same)
-                break
-            else:
-                print(res)
-                break
-        else:
-            if olimpic[i][1:] != olimpic[i - 1][1:]:
-                res += 1
-            if olimpic[i][1:] == olimpic[i - 1][1:]:
-                same += 1
