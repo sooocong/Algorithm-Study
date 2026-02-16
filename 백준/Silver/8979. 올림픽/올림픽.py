2ee09bb0c272d111163990893b8760fa4368095e
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-16 15:28:05 +0900

    [Silver V] Title: 올림픽, Time: 36 ms, Memory: 32412 KB, Score: 45 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.py
+++ 백준/Silver/8979. 올림픽/올림픽.py
@@ -3,6 +3,7 @@ input = sys.stdin.readline
 
 N, K = map(int, input().split())
 olimpic = []
+
 for _ in range(N):
     name, g, s, b = input().split()
     name, g, s, b = map(int, (name, g, s, b))
@@ -11,9 +12,23 @@ for _ in range(N):
 olimpic.sort(key=lambda x: (x[1], x[2], x[3]), reverse = True)
 
 res = 1
+same = 0
 for i in range(len(olimpic)):
-    if olimpic[i][0] == K:
-        print(res)
-    elif i >= 1:
-        if olimpic[i][1:] != olimpic[i - 1][1:]:
-            res += 1
+    if i == 0:
+        if olimpic[i][0] == K:
+            print(1)
+            break
+    else:
+        if olimpic[i][0] == K:
+            if olimpic[i][1:] != olimpic[i - 1][1:]:
+                res += 1
+                print(res + same)
+                break
+            else:
+                print(res)
+                break
+        else:
+            if olimpic[i][1:] != olimpic[i - 1][1:]:
+                res += 1
+            if olimpic[i][1:] == olimpic[i - 1][1:]:
+                same += 1
