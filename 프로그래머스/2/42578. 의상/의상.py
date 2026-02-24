0ad82ce6a1bc68fe178293399daa8a9fe7a9d24f
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-24 14:46:12 +0900

    [level 2] Title: 의상, Time: 0.02 ms, Memory: 9.24 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.py
+++ 프로그래머스/2/42578. 의상/의상.py
@@ -1,15 +1,14 @@
 def solution(clothes):
     clothes_dict = {}
-    ans = 1
+    
     for clo in clothes:
         if clo[1] not in clothes_dict:
             clothes_dict[clo[1]] = 1
         else:
             clothes_dict[clo[1]] += 1
             
-    clothes_dict = list(clothes_dict.values())
-    
-    for i in range(len(clothes_dict)):
-        ans *= (clothes_dict[i] + 1)
-        
+    ans = 1
+    for i, v in clothes_dict.items():
+        ans *= (v + 1)
+            
     return ans - 1
