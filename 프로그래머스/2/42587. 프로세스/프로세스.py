db504cec75653730f88a55263c59d041870ab77e
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-25 17:42:56 +0900

    [level 2] Title: 프로세스, Time: 1.10 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.py
+++ 프로그래머스/2/42587. 프로세스/프로세스.py
@@ -2,16 +2,18 @@ from collections import deque
 
 def solution(priorities, location):
     answer = []
-    queue = deque((i, j) for i, j in enumerate(priorities))
-    
+    queue = deque((idx, v) for idx, v in enumerate(priorities))
+
     while queue:
         process = queue.popleft()
-        
-        if queue and any(process[1] < q[1] for q in queue):
+        if any(process[1] < q[1] for q in queue):
             queue.append(process)
         else:
             answer.append(process)
     
-    for i in answer:
-        if i[0] == location:
-            return answer.index(i) + 1
+    cnt = 1
+    for a in answer:
+        if location == a[0]:
+            return cnt
+        else:
+            cnt += 1
