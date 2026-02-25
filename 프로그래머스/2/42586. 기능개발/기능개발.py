4f7c7edfccbeefa0228f1f60c0c0c332cb75786b
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-25 16:25:03 +0900

    [level 2] Title: 기능개발, Time: 0.03 ms, Memory: 9.18 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.py
+++ 프로그래머스/2/42586. 기능개발/기능개발.py
@@ -1,23 +1,22 @@
 def solution(progresses, speeds):
-    
+    answer = []
+    remain_stack = []
     for i in range(len(progresses)):
-        mod = (100 - progresses[i]) % speeds[i]
-        progresses[i] = (100 - progresses[i]) // speeds[i]
-        if mod != 0:
-            progresses[i] += 1
-            
-    result = []
-    answer = 1
-    max_num = 0
-    for i in range(len(progresses) - 1):
-        max_num = max(max_num, progresses[i])
-        if progresses[i] < progresses[i + 1] and max_num < progresses[i + 1]:
-            result.append(answer)
-            answer = 1
+        t = 100 - progresses[i]
+        if t % speeds[i] == 0:
+            remain_stack.append(t // speeds[i])
         else:
-            answer += 1
-            
-    if len(progresses) != sum(result):
-        result.append(len(progresses) - sum(result))
+            remain_stack.append(t // speeds[i] + 1)
     
-    return result
+    max_num = remain_stack[0]
+    cnt = 1
+    for remain in remain_stack[1:]:
+        if max_num >= remain:
+            cnt += 1
+        else:
+            answer.append(cnt)
+            max_num = remain
+            cnt = 1
+    answer.append(cnt)
+    
+    return answer
