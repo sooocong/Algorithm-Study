523fab06df93885b177e60c834eaf18d9ccb6d09
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-27 17:41:16 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,13 +1,9 @@
 def solution(array, commands):
-    a = len(commands)
-    letter = []
-    answer = []
+    result = []
     
-    for i in range(a):
-        letter.append(array[commands[i][0] - 1 : commands[i][1]])
-        letter[i] = sorted(letter[i])
-        
-    for i in range(a):
-        answer.append(letter[i][commands[i][2] - 1])
-        
-    return answer
+    for com in commands:
+        k = array[com[0] - 1:com[1]]
+        k.sort()
+        result.append(k[com[2] - 1])
+    
+    return result
