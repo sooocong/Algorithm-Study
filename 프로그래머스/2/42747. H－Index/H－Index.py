8c264f189e740f0104a14501b386ae9fbd1d2329
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-28 17:54:26 +0900

    [level 2] Title: H-Index, Time: 0.10 ms, Memory: 9.07 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.py
+++ 프로그래머스/2/42747. H－Index/H－Index.py
@@ -1,11 +1,9 @@
 def solution(citations):
-    citations.sort(reverse=True)
-    h = 0
-
-    for i in range(len(citations)):
-        if citations[i] >= i + 1:
-            h = i + 1
-        else:
-            break
-
-    return h
+    citations.sort()
+    n = len(citations)
+        
+    for i in range(n):
+        h = n - i
+        if citations[i] >= h:
+            return h
+    return 0
