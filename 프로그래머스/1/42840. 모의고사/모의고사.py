72609f228c69d75db07cf0cf5ca60a3d7f97f02c
soobin <79037489+sooocong@users.noreply.github.com>
2026-02-28 18:37:15 +0900

    [level 1] Title: 모의고사, Time: 3.98 ms, Memory: 9.38 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.py
+++ 프로그래머스/1/42840. 모의고사/모의고사.py
@@ -1,17 +1,26 @@
 def solution(answers):
-    one = [1, 2, 3, 4, 5]
-    two = [2, 1, 2, 3, 2, 4, 2, 5]
-    thr = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    cnt = [0, 0, 0]
-    
-    for i in range(len(answers)):
-        if answers[i] == one[i % len(one)]:
-            cnt[0] += 1
-        if answers[i] == two[i % len(two)]:
-            cnt[1] += 1
-        if answers[i] == thr[i % len(thr)]:
-            cnt[2] += 1
-            
-    max_cnt = max(cnt)
+    ans = []
+    answer = []
 
-    return [i + 1 for i, v in enumerate(cnt) if v == max_cnt]
+    a = [1, 2, 3, 4, 5]
+    b = [2, 1, 2, 3, 2, 4, 2, 5]
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    
+    cnt_a, cnt_b, cnt_c = 0, 0, 0
+    for i in range(len(answers)):
+        if answers[i] == a[i % len(a)]:
+            cnt_a += 1
+        if answers[i] == b[i % len(b)]:
+            cnt_b += 1
+        if answers[i] == c[i % len(c)]:
+            cnt_c += 1
+            
+    ans.append(cnt_a)
+    ans.append(cnt_b)
+    ans.append(cnt_c)
+    max_ans = max(ans)
+    
+    for i in range(len(ans)):
+        if ans[i] == max_ans:
+            answer.append(i + 1)
+    return answer
