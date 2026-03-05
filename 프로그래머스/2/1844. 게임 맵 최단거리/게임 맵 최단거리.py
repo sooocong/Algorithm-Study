ffeaeb9c612810e60295b07f6ec7b228a87f5db5
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-05 14:48:53 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 13.26 ms, Memory: 9.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.py
@@ -4,35 +4,32 @@ def solution(maps):
     n = len(maps)
     m = len(maps[0])
     
-    # 방향: 우, 좌, 하, 상
-    direction = [(0,1), (0,-1), (1,0), (-1,0)]
-    
-    # BFS 큐 초기화 (시작점)
+    dist = [[-1] * m for _ in range(n)]
+    dist[0][0] = 1
     q = deque()
     q.append((0, 0))
     
-    # 시작점 방문 처리 (거리 = 1)
-    # maps[0][0]은 원래 1이지만, "방문했다"는 의미로 그대로 사용
-    # 이후 방문한 칸은 2, 3, 4 ... 로 바뀜
+    dx = [-1, 1, 0, 0]
+    dy = [0, 0, -1, 1]
     
     while q:
         x, y = q.popleft()
         
-        # 도착 지점이면 바로 반환 (BFS라서 최단)
-        if x == n - 1 and y == m - 1:
-            return maps[x][y]
-        
-        for dx, dy in direction:
-            nx = x + dx
-            ny = y + dy
+        for i in range(4):
             
-            # 맵 범위 체크
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
             if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
+            if dist[nx][ny] != -1:
+                continue
+            if maps[nx][ny] == 0:
+                continue
             
-            # 아직 방문 안 한 길(1)만 이동
-            if maps[nx][ny] == 1:
-                maps[nx][ny] = maps[x][y] + 1
-                q.append((nx, ny))
-    
-    return -1
+            dist[nx][ny] = dist[x][y] + 1
+            
+            # 다음에 검사할 값 삽입
+            q.append((nx, ny))
+            
+    return dist[n-1][m-1]
