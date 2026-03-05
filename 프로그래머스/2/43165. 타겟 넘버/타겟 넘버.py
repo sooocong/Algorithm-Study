bd468ae4357c3779cc6e899caea14e847ad950cf
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-05 12:47:47 +0900

    [level 2] Title: 타겟 넘버, Time: 3304.26 ms, Memory: 246 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,18 @@
-def solution(numbers, target):
-    answer = 0
-    index = 0
-    current_sum = 0
+from itertools import product
 
-    def dfs(index, current_sum):
-        nonlocal answer
-        
-        if index == len(numbers):
-            if current_sum == target:
-                answer += 1
-        else:
-            dfs(index + 1, current_sum + numbers[index])
-            dfs(index + 1, current_sum - numbers[index])
-               
-    dfs(0, 0)
+def solution(numbers, target):
+    a = [-1, 1]
+    case_list = []
     
-    return answer
+    for case in product(a, repeat = len(numbers)):
+        case_list.append(list(case))
+
+    ans = 0
+    for i in range(2 ** len(numbers)):
+        cnt = 0
+        for j in range(len(numbers)):
+            cnt += numbers[j] * case_list[i][j]
+        if cnt == target:
+            ans += 1
+        
+    return ans
