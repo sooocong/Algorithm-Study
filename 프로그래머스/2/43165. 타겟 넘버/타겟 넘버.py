0971179d8287845f92e18d1d4e4c1450951d5a07
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-09 17:22:55 +0900

    [level 2] Title: 타겟 넘버, Time: 447.63 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -1,18 +1,12 @@
-from itertools import product
-
 def solution(numbers, target):
-    a = [-1, 1]
-    case_list = []
+    n = len(numbers)
     
-    for case in product(a, repeat = len(numbers)):
-        case_list.append(list(case))
-
-    ans = 0
-    for i in range(2 ** len(numbers)):
-        cnt = 0
-        for j in range(len(numbers)):
-            cnt += numbers[j] * case_list[i][j]
-        if cnt == target:
-            ans += 1
+    def dfs(idx, total):
+        if n == idx:
+            if total == target:
+                return 1
+            else:
+                return 0
+        return dfs(idx+1, total + numbers[idx]) + dfs(idx+1, total - numbers[idx])
         
-    return ans
+    return dfs(0, 0)
