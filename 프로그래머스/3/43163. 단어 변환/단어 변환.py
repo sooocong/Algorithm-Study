c3684a9512c1fa724acf72d900e7a42faf3d5b1b
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-11 19:10:01 +0900

    [level 3] Title: 단어 변환, Time: 0.42 ms, Memory: 9.32 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.py
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.py
@@ -1,22 +1,19 @@
 from collections import deque
 
 def solution(begin, target, words):
-    if target not in words:
-        return 0
-    
+    q = deque()
+    q.append((begin, 0))
+    visited = [False] * len(words)
+        
     def can_change(a, b):
         diff = 0
         for i in range(len(a)):
             if a[i] != b[i]:
                 diff += 1
         return diff == 1
-    
-    visited = [False] * len(words)
-    queue = deque()
-    queue.append((begin, 0))
-    
-    while queue:
-        current, step = queue.popleft()
+        
+    while q:
+        current, step = q.popleft()
         
         if current == target:
             return step
@@ -24,6 +21,7 @@ def solution(begin, target, words):
         for i in range(len(words)):
             if not visited[i] and can_change(current, words[i]):
                 visited[i] = True
-                queue.append((words[i], step + 1))
-    
+                q.append((words[i], step + 1))
+            
     return 0
+            
