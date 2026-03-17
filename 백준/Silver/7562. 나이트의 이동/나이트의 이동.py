98c83e7eaf4bee1540243b1785a7457d57f33114
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-17 10:20:19 +0900

    [Silver I] Title: 나이트의 이동, Time: 1124 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.py
@@ -2,32 +2,41 @@ from collections import deque
 import sys
 input = sys.stdin.readline
 
-dx = [-2, -2, -1, -1, 1, 1, 2, 2]
-dy = [-1, 1, -2, 2, -2, 2, -1, 1]
+def solution(t):
 
-def bfs(l, sx, sy, ex, ey):
-    visited = [[0] * l for _ in range(l)]
-    q = deque([(sx, sy)])
-    visited[sx][sy] = 1
+    dx = [-2, -2, -1, -1, 1, 1, 2, 2]
+    dy = [-1, 1, -2, 2, -2, 2, -1, 1]
 
-    while q:
-        x, y = q.popleft()
+    def bfs(l, sx, sy, ex, ey):
+        q = deque()
+        q.append((sx, sy))
 
-        if x == ex and y == ey:
-            return visited[x][y] - 1
+        visited = [[0] * l for _ in range(l)]
+        visited[sx][sy] = 1
 
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
+        while q:
+            x, y = q.popleft()
+            if x == ex and y == ey:
+                return visited[x][y] - 1
 
-            if 0 <= nx < l and 0 <= ny < l and visited[nx][ny] == 0:
-                visited[nx][ny] = visited[x][y] + 1
-                q.append((nx, ny))
+            for i in range(8):
+                nx, ny = x + dx[i], y + dy[i]
+
+                if 0 <= nx < l and 0 <= ny < l and visited[nx][ny] == 0:
+                    visited[nx][ny] = visited[x][y] + 1
+                    q.append((nx, ny))
+
+    answer = []
+
+    for _ in range(t):
+        l = int(input())
+        sx, sy = map(int, input().split())
+        ex, ey = map(int, input().split())
+        answer.append(bfs(l, sx, sy, ex, ey))
+
+    return answer
 
 t = int(input())
-for _ in range(t):
-    l = int(input())
-    sx, sy = map(int, input().split())
-    ex, ey = map(int, input().split())
-
-    print(bfs(l, sx, sy, ex, ey))
+result = solution(t)
+for r in result:
+    print(r)
