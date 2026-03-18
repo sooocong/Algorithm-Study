88414159c74d20f885a4f7276776354400382c1a
soobin <79037489+sooocong@users.noreply.github.com>
2026-03-18 17:08:10 +0900

    [Silver II] Title: 유기농 배추, Time: 60 ms, Memory: 34976 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.py
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.py
@@ -1,32 +1,41 @@
 from collections import deque
+import sys
+input = sys.stdin.readline
 
-dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+def bfs():
+    t = int(input())
 
-t = int(input())
-for ___ in range(t):
-    m, n, k = map(int,input().split())
-    arr = [[0]*m for _ in range(n)]
-    
-    for i in range(k):
-        y, x = map(int,input().split())
-        arr[x][y] = 1
-        
-    check = [[0]*m for _ in range(n)]
-    val = 1
+    for _ in range(t):
+        m, n, k = map(int, input().split())
+        rect = [[0] * m for _ in range(n)]
+        visited = [[False] * m for _ in range(n)]
 
-    q = deque()
-    for i in range(n):
-        for j in range(m):
-            if check[i][j] == 0 and arr[i][j] == 1:
-                check[i][j] = val
-                q.append((i,j))
-                
-                while q:
-                    x, y = q.popleft()
-                    for k in range(4):
-                        nx, ny = x + dx[k], y + dy[k]
-                        if 0 <= nx < n and 0 <= ny < m and arr[nx][ny] and not check[nx][ny]:
-                            check[nx][ny] = val
-                            q.append((nx,ny))
-                val += 1
-    print(val - 1)
+        dx = [0, 0, -1, 1]
+        dy = [-1, 1, 0, 0]
+
+        for _ in range(k):
+            x, y = map(int, input().split())
+            rect[y][x] = 1
+
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if rect[i][j] == 1 and not visited[i][j]:
+                    ans += 1
+                    queue = deque()
+                    queue.append((i, j))
+                    visited[i][j] = True
+
+                    while queue:
+                        x, y = queue.popleft()
+
+                        for k in range(4):
+                            nx, ny = x + dx[k], y + dy[k]
+                            if 0 <= nx < n and 0 <= ny < m and rect[nx][ny] == 1 and not visited[nx][ny]:
+                                visited[nx][ny] = True
+                                queue.append((nx, ny))
+
+
+        print(ans)
+
+bfs()
