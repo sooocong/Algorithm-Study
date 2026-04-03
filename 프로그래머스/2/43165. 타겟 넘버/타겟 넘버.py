55addc9bfe17898dbfc4c652cc19b7431af6e019
soobin <79037489+sooocong@users.noreply.github.com>
2026-04-03 15:18:45 +0900

    [level 2] Title: 타겟 넘버, Time: 405.36 ms, Memory: 9.26 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -2,11 +2,12 @@ def solution(numbers, target):
     n = len(numbers)
     
     def dfs(idx, total):
-        if n == idx:
+        if idx == n:
             if total == target:
                 return 1
             else:
                 return 0
-        return dfs(idx+1, total + numbers[idx]) + dfs(idx+1, total - numbers[idx])
+            
+        return dfs(idx+1, total - numbers[idx]) + dfs(idx+1, total + numbers[idx])
         
     return dfs(0, 0)
