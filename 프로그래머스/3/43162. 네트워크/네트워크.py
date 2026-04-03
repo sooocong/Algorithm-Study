40f472c823375b93c58e03369a2c7166836fd960
soobin <79037489+sooocong@users.noreply.github.com>
2026-04-03 16:17:58 +0900

    [level 3] Title: 네트워크, Time: 0.50 ms, Memory: 9.35 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.py
+++ 프로그래머스/3/43162. 네트워크/네트워크.py
@@ -1,14 +1,14 @@
 def solution(n, computers):
-    answer = 0
     visited = [False] * n
+    answer = 0
     
     def dfs(node):
         visited[node] = True
         
-        for next_node in range(n):
-            if computers[node][next_node] == 1 and not visited[next_node]:
-                dfs(next_node)
-        
+        for i in range(n):
+            if computers[node][i] == 1 and not visited[i]:
+                dfs(i)
+    
     for i in range(n):
         if not visited[i]:
             dfs(i)
